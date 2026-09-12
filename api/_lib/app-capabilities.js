bc7f9f496e4070d670cf3005ffb8a42208eae89c
Craig Jordan <60245269+Odysseusss@users.noreply.github.com>
2026-09-12 10:54:24 -0400

    Include entitlement tier in capabilities

--- api/_lib/app-capabilities.js
+++ api/_lib/app-capabilities.js
@@ -6,12 +6,14 @@ export const APP_TIERS = Object.freeze({
 export function capabilitiesFor(tier) {
   if (tier === APP_TIERS.ANNUAL_FULL) {
     return {
+      tier: APP_TIERS.ANNUAL_FULL,
       maxSavedDrawings: null,
       showAds: false,
       checkoutEligible: false,
     };
   }
   return {
+    tier: APP_TIERS.FREE,
     maxSavedDrawings: 3,
     showAds: true,
     checkoutEligible: true,
--- test/app-capabilities.test.js
+++ test/app-capabilities.test.js
@@ -4,6 +4,7 @@ import { APP_TIERS, capabilitiesFor } from "../api/_lib/app-capabilities.js";
 
 test("free accounts receive three saves and ad eligibility", () => {
   assert.deepEqual(capabilitiesFor(APP_TIERS.FREE), {
+    tier: APP_TIERS.FREE,
     maxSavedDrawings: 3,
     showAds: true,
     checkoutEligible: true,
@@ -12,6 +13,7 @@ test("free accounts receive three saves and ad eligibility", () => {
 
 test("annual full accounts receive unlimited saves without ads", () => {
   assert.deepEqual(capabilitiesFor(APP_TIERS.ANNUAL_FULL), {
+    tier: APP_TIERS.ANNUAL_FULL,
     maxSavedDrawings: null,
     showAds: false,
     checkoutEligible: false,
